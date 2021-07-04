fa5299ecc62f0829e3e5183043483aedb3d889b2
Aiden Langley <me@aidenlangley.com>
2021-07-04 15:26:31 +1200

    Progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
-use std::{collections::HashMap, convert::TryFrom};
+use std::{
+    borrow::Borrow,
+    collections::HashMap,
+    convert::TryFrom,
+    fmt::{write, Display},
+};
 
 use chrono::{Date, DateTime, Duration, Local, Utc};
+use math::round::floor;
 
 /**
 Provides a context-aware `DateTime` object; a given `DateTime` is made aware in the context of the
@@ -13,7 +19,7 @@ years and 9 months`.
 Aliased as `DueDateTime` out of the box in case that makes more sense in your context.
 */
 #[derive(Debug, Clone)]
-pub struct Elapsed<'a> {
+pub struct Elapsed {
     /**
     The `DateTime` that gives this meaningful context, will default to `now`, but can be modified to
     get elapsed time between dates.
@@ -36,13 +42,14 @@ pub struct Elapsed<'a> {
     Key being the sec/min/hour/day, etc. identifier.
     Value being a tuple of the `diff` value as a string in pos 0, and numeric value in pos 1.
     */
-    cache: HashMap<TimeFrame, (&'a str, usize)>, // format: &'static str,
+    cache: HashMap<TimeFrame, (String, i64)>, // format: &'static str,
 }
 
-/** Aliasing `Elapsed` as `DueDateTime` because both make sense, depending on use-case. */
-pub type DueDateTime<'a> = Elapsed<'a>;
+/** Aliasing `Elapsed` because they make sense, depending on use-case. */
+pub type DueDateTime = Elapsed;
+pub type TimeBetween = Elapsed;
 
-impl Elapsed<'_> {
+impl Elapsed {
     /** Construct a new object. */
     pub fn new(datetime: DateTime<Local>) -> Self {
         let datetime_context = Local::now();
@@ -148,37 +155,171 @@ impl Elapsed<'_> {
         self.duration = self.datetime.signed_duration_since(self.datetime_context);
         self.passed = self.datetime.le(&self.datetime_context);
     }
+
+    /**
+    Default behaviour currently. Discards "irrelevant" time frames, for example if date is due in
+    more than a year, we'll only store `1y 6m` as opposed to `1y 6m 2w 4d`.
+    */
+    pub fn process_all(&mut self) {
+        /*
+        All absolute values, we can assume values are below zero later on when we check `passed`,
+        whilst we're building the str that represents time elapsed, we aren't concerned with past or
+        future.
+
+        `chrono` returns whole weeks, days, etc. so no rounding is present.
+        */
+        let diff = self.duration;
+        let weeks = diff.num_weeks().abs();
+        let days = diff.num_days().abs();
+        let hours = diff.num_hours().abs();
+        let minutes = diff.num_minutes().abs();
+        let seconds = diff.num_seconds().abs();
+
+        if weeks > 0 {
+            if weeks > 0 && weeks < 4 {
+                /* In n weeks, simples. */
+                self.cache_insert(TimeFrame::Week, weeks);
+            } else
+            /* Months: */
+            {
+                /* Round down for months, easy for us to add remaining weeks. */
+                let months = floor((weeks / 4) as f64, 0) as i64;
+                /*
+                Get remaining weeks, e.g.:
+                6w [1m (+2w, rounded off)] - (1m * 4w) = 2w
+                */
+                let weeks_remaining = weeks - months * 4;
+                if months < 12
+                /* Less than a year: */
+                {
+                    self.cache_insert(TimeFrame::Month, months);
+                    self.cache_insert(TimeFrame::Week, weeks_remaining);
+                } else
+                /* Potentially multiple years */
+                {
+                    let years = floor((months / 12) as f64, 0) as i64;
+                    let months_remaining = months - years * 12;
+                    self.cache_insert(TimeFrame::Year, years);
+                    self.cache_insert(TimeFrame::Month, months_remaining);
+                }
+            }
+        } else if days > 0
+        /* and weeks are 0. */
+        {
+            self.cache_insert(TimeFrame::Day, days);
+        } else if hours >= 4
+        /* and days are 0. */
+        {
+            self.cache_insert(TimeFrame::Hour, hours);
+        } else if minutes >= 60
+        /* and in less than 4 hours. */
+        {
+            self.cache_insert(TimeFrame::Minute, minutes - hours * 60);
+        } else if minutes >= 5
+        /* and in less than an hour. */
+        {
+            self.cache_insert(TimeFrame::Minute, minutes);
+        } else if seconds > 0
+        /* and less 5 minutes away. */
+        {
+            /* Pads left with 0s: format!("{:0>1}:{:0>1}s", min, sec_remaining) */
+            self.cache_insert(TimeFrame::Minute, minutes);
+            self.cache_insert(TimeFrame::Second, seconds - minutes * 60);
+        }
+    }
+
+    fn cache_insert(&mut self, k: TimeFrame, v: i64) {
+        let tup = (format!("{}{}", v, k.abbrev()), v);
+        let val = self.cache.entry(k).or_insert(tup);
+        val.1 = v;
+    }
+
+    pub fn years(&mut self) -> Option<&(String, i64)> {
+        let years = floor((self.duration.num_weeks() / 52) as f64, 0) as i64;
+        if years.ne(&0) {
+            let tf = TimeFrame::Year;
+            self.cache_insert(tf, years);
+            self.cache.get(&tf)
+        } else {
+            None
+        }
+    }
+
+    pub fn months(&mut self) -> Option<&(String, i64)> {
+        let months = floor((self.duration.num_weeks() / 4) as f64, 0) as i64;
+        let tf = TimeFrame::Month;
+        self.cache_insert(tf, months);
+        self.cache.get(&tf)
+    }
+
+    pub fn weeks(&mut self) {}
+
+    pub fn days(&mut self) {}
+
+    pub fn hours(&mut self) {}
+
+    pub fn minutes(&mut self) {}
+
+    pub fn seconds(&mut self) {}
 }
 
-impl From<DateTime<Local>> for Elapsed<'_> {
+impl Display for Elapsed {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut vec: Vec<&str> = Vec::new();
+        if let Some(years) = self.cache.get(&TimeFrame::Year) {
+            vec.push(&years.0);
+        }
+        if let Some(months) = self.cache.get(&TimeFrame::Month) {
+            vec.push(&months.0);
+        }
+        if let Some(weeks) = self.cache.get(&TimeFrame::Week) {
+            vec.push(&weeks.0);
+        }
+        if let Some(days) = self.cache.get(&TimeFrame::Day) {
+            vec.push(&days.0);
+        }
+        if let Some(hours) = self.cache.get(&TimeFrame::Hour) {
+            vec.push(&hours.0);
+        }
+        if let Some(minutes) = self.cache.get(&TimeFrame::Minute) {
+            vec.push(&minutes.0);
+        }
+        if let Some(seconds) = self.cache.get(&TimeFrame::Second) {
+            vec.push(&seconds.0);
+        }
+        write!(f, "{}", vec.join(" "))
+    }
+}
+
+impl From<DateTime<Local>> for Elapsed {
     /** Construct _from_ localised `DateTime`. */
     fn from(datetime: DateTime<Local>) -> Self {
         Self::new(datetime)
     }
 }
 
-impl From<Date<Local>> for Elapsed<'_> {
+impl From<Date<Local>> for Elapsed {
     /** Construct _from_ localised `Date`. */
     fn from(date: Date<Local>) -> Self {
         Self::new_from_date(date)
     }
 }
 
-impl From<DateTime<Utc>> for Elapsed<'_> {
+impl From<DateTime<Utc>> for Elapsed {
     /** Construct _from_ UTC `DateTime`. */
     fn from(datetime: DateTime<Utc>) -> Self {
         Self::new_then_localize(datetime)
     }
 }
 
-impl From<Date<Utc>> for Elapsed<'_> {
+impl From<Date<Utc>> for Elapsed {
     /** Construct _from_ UTC `Date`. */
     fn from(date: Date<Utc>) -> Self {
         Self::new_from_date_then_localize(date)
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 enum TimeFrame {
     /*
     Tempted to leave millisecond out because by virtue this crate isn't dealing with micro and nano
@@ -262,13 +403,13 @@ impl TryFrom<char> for TimeFrame {
 }
 
 pub trait Abbreviate {
-    fn abbrev(&self) -> &str;
-    fn abbrev_short(&self) -> &str;
+    fn abbrev(&self) -> &'static str;
+    fn abbrev_short(&self) -> &'static str;
 }
 
 impl Abbreviate for TimeFrame {
     /** Abbreviate `TimeFrame` to reasonably short string. */
-    fn abbrev(&self) -> &str {
+    fn abbrev(&self) -> &'static str {
         match self {
             TimeFrame::MilliSecond => "ms",
             TimeFrame::Second => "sec",
@@ -285,7 +426,7 @@ impl Abbreviate for TimeFrame {
     Abbreviate `TimeFrame` to a still sensibly short string, mostly just a char except when there
     are clashes (ms, min, month).
     */
-    fn abbrev_short(&self) -> &str {
+    fn abbrev_short(&self) -> &'static str {
         match self {
             TimeFrame::MilliSecond => "ms",
             TimeFrame::Second => "s",
